5f0a0de4aff058fd7885cf6d2b3d1db23d8b8e86
Nicolas Quach <nicolasquach@DN525ehg.SUNet>
2017-06-13 13:57:46 -0700

    Updated readme, and added comments to interpolater.py

--- interpolater.py
+++ interpolater.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+#THIS INTERPOLATION IS ONLY FOR 200µL of liquid in 96 well plate well.
+#Data from calibration experiment
 A200 = [0, 0.0088, .0193, .0405, .0797, .123, .1673, .2128, .2349, .3481]
 Aspec = [0, .072, .152, .297, .570, .783, .948, 1.088, 1.199, 1.367]
 
 A200 = np.asarray(A200)
 Aspec = np.asarray(Aspec)
 
+#Plate reader OD600 to interpolate into a spectrophotometer reading
 to_interp = 0.12
 
 print "Interpolating plate reader OD600 of " + str(to_interp) + ":"
